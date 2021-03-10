d89c6e409554cb2a8784e0e17878b24839cbf182
justbk <249396768@qq.com>
2021-03-10 12:23:46 +0800

    增加表名称的变量表示，防止表名称变更引起的大量修改

--- src/main/java/bin/common/mapper/JsonbMapper.java
+++ src/main/java/bin/common/mapper/JsonbMapper.java
@@ -5,6 +5,8 @@
 package bin.common.mapper;
 
 import bin.common.vo.JsonbVo;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 import java.util.Map;
@@ -18,10 +20,16 @@ import java.util.Map;
  * @since 2021/3/9
  */
 public interface JsonbMapper {
+    @Update("create table if not exists t_jsonb(id int primary key, data jsonb)")
     void createTable();
+
+    @Update("drop table if exists t_jsonb")
     void dropTable();
-    int insertOne(JsonbVo vo);
+
+    @Select("select case when max(id) is null then 0 else max(id)+1 end as maxId  from t_jsonb")
     int selectMaxId();
+
+    int insertOne(JsonbVo vo);
     JsonbVo selectJsonb(int id);
     List<JsonbVo> selectJsonbAll();
 }
